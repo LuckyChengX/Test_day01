3fcabf58d71f0a4a323dc6f1c2e6323be50298dc
LuckyChengX <917359032@qq.com>
2022-07-12 16:59:08 +0800

    Changes

---
+++ Test_day01/src/day_01/test1.java
@@ -0,0 +1,12 @@
+package day_01;
+
+import java.util.Scanner;
+
+public class test1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("请输入一个整数：");
+        int n = scan.nextInt();
+        System.out.println("您输入的整数是："+n);
+    }
+}
